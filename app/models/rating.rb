90a552cd8953ab2c7c119814df15e85defc90c0c
@pguskov <redmandarin@gmail.com>
2014-12-07 00:29:35 +0300

    passed Vote specs

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,7 @@
 class Rating < ActiveRecord::Base
+  validates :model_id, :model_name, :user_id, presence: true
+
+  belongs_to :user
+  belongs_to :question
+  belongs_to :answer
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Vote, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :rating_id }
+  it { should validate_presence_of :value }
+  it { should belong_to :user }
+  it { should belong_to :rating }
 end
