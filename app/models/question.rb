04a745536697eac839dc639f59c4b79f180e6083
@pguskov <redmandarin@gmail.com>
2014-12-09 02:24:55 +0300

    polymorphic comments

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,8 @@ class Question < ActiveRecord::Base
   validates :title, :body, presence: true
 
   has_many :answers
-  has_many :comments
+  has_many :comments, as: :commentable
   belongs_to :user
-  has_one :rating
+  has_one :rating, as: :rateable
 
 end
