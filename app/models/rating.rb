908df74f0621dcf28bd2b69c2b6269e6768be61d
@pguskov <redmandarin@gmail.com>
2014-12-09 02:29:54 +0300

    polymorphic rateable

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,9 +1,7 @@
 class Rating < ActiveRecord::Base
   validates :model_id, :model_name, presence: true
 
-  belongs_to :user
-  belongs_to :question
-  belongs_to :answer
+  belongs_to :rateable, polymorphic: true
   has_many :votes
 
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Rating, :type => :model do
-  it { should validate_presence_of :model_id }
-  it { should validate_presence_of :model_name }
-  it { should belong_to :user }
-  it { should belong_to :question }
-  it { should belong_to :answer }
+  it { should belong_to :rateable }
   it { should have_many :votes }
 
   it "should have default value 0" do
