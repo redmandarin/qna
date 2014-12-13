493b4e0ded94adadbf83b35a0866b1341fdecb5a
@pguskov <redmandarin@gmail.com>
2014-12-13 13:55:00 +0300

    wait

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
   has_one :rating, as: :rateable
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :body, :user_id, :question_id, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,9 @@
 class Question < ActiveRecord::Base
-  has_many :answers
-  has_many :comments, as: :commentable
-  has_many :taggings
+  has_many :answers, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
-  has_one :rating, as: :rateable
+  has_one :rating, as: :rateable, dependent: :destroy
   belongs_to :user
 
   validates :title, :body, presence: true
