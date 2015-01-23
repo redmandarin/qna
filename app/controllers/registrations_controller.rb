dc15e830f2892e240a9fbf1cd501297f0a836bef
@pguskov <redmandarin@gmail.com>
2015-01-23 10:08:06 +0300

    registration controller test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,6 @@ class RegistrationsController < Devise::RegistrationsController
       oauth_hash = OmniAuth::AuthHash.new(provider: session[:oauth_provider], uid: session[:oauth_uid], info: { email: params[:user][:email] })
       @user = User.find_for_oauth(oauth_hash)
       sign_in_and_redirect @user, event: :authentication
-      # resource.authorizations.create(provider: session[:oauth_provider], uid: session[:oauth_uid])
     else
       super
     end
---
+++ spec/controllers/registrations_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+Rspec.describe RegistrationsController, type: :controller do
+  let(:user) { create(:user) }
+  
+  describe "POST create" do
+    context 'with session provider' do
+      
+    end
+  end
+end
