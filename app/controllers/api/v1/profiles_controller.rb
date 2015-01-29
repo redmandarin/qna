63d0fc36072ebff41a86fd81343941335a6c6117
@pguskov <redmandarin@gmail.com>
2015-01-29 17:36:36 +0300

    /profiles

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     respond_with current_resource_owner
   end
+
+  def index
+    respond_with(@users = User.where(["id != ?", current_resource_owner.id]))
+  end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -38,4 +38,40 @@ describe 'Profile API' do
       end
     end
   end
+
+  describe 'GET /' do
+    context 'unauthorized' do
+      it 'returns 401 if no given access_token' do
+        get '/api/v1/profiles', format: :json
+        expect(response.status).to eq(401)
+      end
+
+      it 'returns 401 error if access_token is invalid' do
+        get '/api/v1/profiles', format: :json, access_token: '12345'
+      end
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3) }
+      let(:user) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id)}
+
+      before { get '/api/v1/profiles', access_token: access_token.token, format: :json }
+
+      it 'returns 200' do
+        puts response.body
+        expect(response).to be_success
+      end
+
+      it 'rerurns list of users' do
+        expect(response.body).to have_json_size(2).at_path("profiles")
+      end
+
+      %w(id email created_at updated_at).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(users[1].send(attr.to_sym).to_json).at_path("profiles/0/#{attr}")
+        end
+      end
+    end
+  end
 end
