8f9dd02aebc6ed60d22bcd7fb5fc9db9f4566536
Petr Guskov <redmandarin@gmail.com>
2015-02-17 20:42:35 +0300

    private_pub

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, '/home/deployer/qna'
 set :deploy_user, 'deployer'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env', 'config/private_pub_thin.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -22,4 +22,39 @@ namespace :deploy do
   end
 
   after :publishing, :restart
+end
+
+namespace :private_pub do
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, 'exec thin -C config/private_pub_thin.yml start'
+        end
+      end
+    end
+  end
+
+  desc 'Stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, 'exec thin -C config/private_pub_thin.yml stop'
+        end
+      end
+    end
+  end
+
+  desc 'restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, 'exec thin -C config/private_pub_thin.yml restart'
+        end
+      end
+    end
+  end
 end
