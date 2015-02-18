6acf8ad9f136c2eb87229e4c64fc26fd0f8d6767
Petr Guskov <redmandarin@gmail.com>
2015-02-18 22:18:46 +0300

    th

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,8 @@ set :deploy_to, '/home/deployer/qna'
 set :deploy_user, 'deployer'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env', 'config/private_pub_thin.yml')
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env', 'config/private_pub_thin.yml', 'searchd.pid', 'production.sphinx.conf')
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env', 'config/private_pub_thin.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env', 'config/private_pub_thin.yml', 'searchd.pid', 'production.sphinx.conf')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'db/sphinx', 'binlog')
